69e118ab956d99147516be21e5067c34eb439576
mbeckius@gmail.com <MBeckius@gmail.com>
2017-06-14 08:19:33 -0400

    more

--- src/app/hero-form.component.ts
+++ src/app/hero-form.component.ts
@@ -2,7 +2,17 @@ import { Component } from '@angular/core';
 import { Hero } from './hero';
 
 @Component({
-    templateUrl : './hero-form.component.html'
+    templateUrl : './hero-form.component.html',
+    styles : [
+        `.ng-valid[required], .ng-valid.required  {
+         border-left: 5px solid #42A948; /* green */
+            }`,
+
+        `.ng-invalid:not(form)  {
+        border-left: 5px solid #a94442; /* red */
+        }`
+    ]
+    
 })
 export class HeroFormComponent {
  
